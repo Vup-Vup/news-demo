b981151792a88d9e62adbef62ac2a0342df6f100
Vup-Vup <vup0162@gmail.com>
2024-11-01 10:57:08 +0700

    last

---
+++ models/item.php
@@ -0,0 +1,12 @@
+<?php
+class Item extends Db
+{
+    public function getAllItems()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM items");
+        $sql->execute();
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items;
+    }
+}
